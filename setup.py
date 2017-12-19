70aa268b9f1914a244fbb37781ab006df87226c7
Jacob Magnusson <m@jacobian.se>
2017-12-19 16:39:26 +0100

    Drop py33 from tests

--- setup.py
+++ setup.py
@@ -80,7 +80,6 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
